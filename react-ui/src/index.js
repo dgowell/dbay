dae4f603bd1706426b3effc5f4054c1ef929bfb6
Joel Cormack <dgowellkomrak@gmail.com>
2022-11-02 17:36:18 -0600

    Migrate files from working demo app

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 window.MDS.init();
 
@@ -17,3 +18,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
