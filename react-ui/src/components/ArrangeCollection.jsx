f570f7767bf3ddbdd6a8f452014ea042a3ededa0
Joel Cormack <dgowellkomrak@gmail.com>
2023-04-10 14:43:10 -0500

    feature/style confirm page

--- react-ui/src/components/ArrangeCollection.jsx
+++ react-ui/src/components/ArrangeCollection.jsx
@@ -7,7 +7,7 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import sent from "../assets/images/ts_1.gif";
-import { useNavigate,useParams,useLocation } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import LoadingButton from "@mui/lab/LoadingButton";
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import { CardHeader } from "@mui/material";
@@ -19,85 +19,54 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import Avatar from "@mui/material/Avatar";
 import Alert from '@mui/material/Alert';
 import { getListingById } from "../database/listing";
-import { getContacts,addContact } from "../minima";
+import { getContacts, addContact } from "../minima";
 
 
 export default function InfoPage() {
   const params = useParams();
   const navigate = useNavigate();
-  const [listing,setListing] = useState();
-  const [seller,setSeller] = useState();
-  const [msg,setMsg] = useState();
-  const [status,setStatus] = useState();
-  const [isContact,setIsContact]=useState(false);
+  const [listing, setListing] = useState();
+  const [seller, setSeller] = useState();
+  const [msg, setMsg] = useState();
+  const [status, setStatus] = useState();
+  const [isContact, setIsContact] = useState(false);
 
   useEffect(() => {
     getListingById(params.id).then(function (result) {
       setListing(result);
-     const contacts =  getContacts();
-     const slr = result.created_by_pk;
-     setSeller(slr);
-     const mls = slr.split("#")[1];
-      Object.keys(contacts).forEach((key,val)=>{
-        if(mls==contacts[key]["extradata"]["mls"]){
+      const contacts = getContacts();
+      const slr = result.created_by_pk;
+      setSeller(slr);
+      const mls = slr.split("#")[1];
+      Object.keys(contacts).forEach((key, val) => {
+        if (mls == contacts[key]["extradata"]["mls"]) {
           setIsContact(true);
         }
       })
     }).catch((e) => console.error(e));
   }, [params.id]);
- 
-async  function handleAdd() {
-   const {msg,status} = await addContact(seller);
-   console.log(msg,status);
-   setStatus(status);
-   setMsg(msg);
+
+  async function handleAdd() {
+    const { msg, status } = await addContact(seller);
+    console.log(msg, status);
+    setStatus(status);
+    setMsg(msg);
   }
 
-return (
-    <div>
-      <Stack spacing={2} sx={{ width: '100%', mt: 2, mb:8,height:'100%' }}>
-        <Stack component="ul" direction="row" spacing={1}
-          sx={{
-            display: "flex",
-            justifyContent: "left",
-            flexWrap: "nowrap",
-            listStyle: "none",
-            margin: 0,
-            padding: 0,
-            overflow: "auto",
-            maxWidth: "400px",
-          }}
-        >
-        <Box width={'100%'} height={'100%'}>
-          <Card sx={{border: "none", boxShadow: "none"}}>
-            <CardHeader style={{fontSize:"24px"}} title={"Arrange Collection"}/>
-            <CardContent>
-                <List>
-                    <ListItem>
-                        <ListItemText primaryTypographyProps={{fontSize: 20,fontWeight:700}} primary={"maxSolo"} secondary={"Add the seller as a contact and get in touch with them using the MaxSolo MiniDapp. "}/>                    
-                    </ListItem>
-                    <ListItem>
-                    {isContact ?   <ListItemText primary="Already a contact"/> :<LoadingButton className={"custom-loading"} style={{color:"#2C2C2C"}}  onClick={()=>handleAdd()} variant="contained">
-                        add Contact
-                      </LoadingButton> }  
-                    </ListItem>
-                   {msg && <ListItem>
-                       <Alert sx={{width:"100%"}} severity={status ? 'success': 'error'} variant="outlined">{msg}</Alert>
-                    </ListItem>}
-                    <ListItem>
-                        <Alert sx={{width:"100%"}} severity='success' variant="outlined">The seller is waiting for you to get in touch</Alert>
-                    </ListItem>
-                </List>
-            </CardContent>
-          </Card>
-          <div style={{justifyContent:"center",width:"100%"}}>
-            <LoadingButton className={"custom-loading"} style={{color:"#2C2C2C",width:"100%",marginTop:"60%",marginBottom:0}}  onClick={()=>navigate("/")} variant="contained">
-                        Close
-            </LoadingButton>
-          </div>
-        </Box>
-        </Stack>
+  return (
+    <Stack sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: 'calc(100vh - 80px)' }}>
+      <Typography sx={{fontSize: "24px", textAlign: "center"}} variant="h1">Arrange Collection</Typography>
+      <Stack spacing={2} sx={{paddingLeft: 2, paddingRight: 2}}>
+        <Typography sx={{  }} variant="h3">MaxSolo</Typography>
+        <Typography sx={{  fontSize: 15, paddingBottom: '30px' }} variant="p">Add the seller as a contact and get in touch with them using the MaxSolo MiniDapp.</Typography>
+        {isContact ? <ListItemText primary="Already a contact" /> : <LoadingButton className={"custom-loading"} color="secondary" variant="contained" onClick={() => handleAdd()}>add Contact</LoadingButton>}
+        {msg && <Alert sx={{ width: "100%" }} severity={status ? 'success' : 'error'} variant="outlined">{msg}</Alert>}
+        <Typography sx={{ textAlign: 'center', marginTop: '15px', flex: 1 }} variant="caption">The seller is expecting you to get in touch.</Typography>
       </Stack>
-    </div>
+        <LoadingButton className={"custom-loading"} color="secondary" onClick={() => navigate("/")} variant="outlined">
+          Close
+        </LoadingButton>
+
+    </Stack>
   );
 }
