3b120cb545ed060de8b30b9131075303b91dc427
muhammad zahoor anwar <xahooranwar@gmail.com>
2023-04-19 12:19:50 -0700

    tabs done

--- react-ui/src/components/ListingListSeller.jsx
+++ react-ui/src/components/ListingListSeller.jsx
@@ -71,7 +71,15 @@ function ListingListSeller() {
       getListings(host.pk)
         .then((data) => {
           setListings(data);
-          console.log(`results: ${data}`);
+          let latest = [...data].sort((a, b) => b.created_at - a.created_at)[0];
+          if(latest.status === 'available' || latest.status === 'pending'){
+            setValue(0);
+          }else if(latest.status === 'sold'){
+            setValue(1);
+          }else if(latest.status === 'completed'){
+            setValue(2);
+          }
+          console.log(`results:`,latest.status);
         })
         .catch((e) => {
           console.error(e);
