03f9db5e44d143a7ad0f99ba67f97c599ea3afef
monthrie <wmontgomery@gmail.com>
2023-05-09 11:43:11 +0100

    coordinates wording change

--- react-ui/src/components/ListingCreate.jsx
+++ react-ui/src/components/ListingCreate.jsx
@@ -430,7 +430,7 @@ export default function ListingCreate() {
                         mt: 3,
                         mb: 3
                       }} elevation={2}>
-                        <Typography>You must add coordinates to your listing in order to make the item available for collection. This makes the listing searchable.</Typography>
+                        <Typography>You can add your approximate coordinates here. Be aware that your location can be estimated to within.</Typography>
                         <LoadingButton color="secondary" className={"custom-loading"} mt={2} loading={loadingCoordinates} variant="outlined" onClick={handleLocation}>Add Coordinates</LoadingButton>
                         {location.latitude !== ''
                           ? <Alert variant="success">coordinates added!</Alert>
