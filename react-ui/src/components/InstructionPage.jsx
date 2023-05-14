ba34db830c72ee935ff1863d7dec50e6160a23e2
monthrie <wmontgomery@gmail.com>
2023-05-15 00:05:56 +0100

    list is put together better

--- react-ui/src/components/InstructionPage.jsx
+++ react-ui/src/components/InstructionPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, ListItemText } from '@mui/material';
 
 const InstructionPage = () => {
   return (
@@ -14,27 +14,26 @@ const InstructionPage = () => {
           </Typography>
         </Box>
         <List>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- This release allows the exchange of real value; be cautious.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- This build is not production-ready and will have major bugs.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- Test with small amounts and trusted contacts.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- Use a 'burner' node with minimal Minima for safety.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- Only whole numbers (integers) can be used for pricing.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- Grant dBay write permissions for smooth operation.</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography variant="body1" sx={{ fontSize: '18px' }}>- Vendors can make items available to non-contact dBay users by obtaining a MAX#ADD on our Discord server.</Typography>
-          </ListItem>
+          {[
+            "This release allows the irriversable exchange of real value; be cautious.",
+            "This build is not production-ready and will have major bugs.",
+            "Test with small amounts and trusted contacts.",
+            "Use a 'burner' node with minimal Minima for safety.",
+            "Only whole numbers (integers) can be used for pricing.",
+            "Grant dBay write permissions for smooth operation, or expect error messages.",
+            "Vendors can obtaining a permanent MAX# on the dBay Discord server."
+          ].map((text, index) => (
+            <ListItem key={index}>
+              <ListItemText>
+                <Typography variant="body1" component="span" sx={{ fontSize: '20px' }}>
+                  &ndash;&nbsp;
+                </Typography>
+                <Typography variant="body1" component="span" sx={{ fontSize: '20px' }}>
+                  {text}
+                </Typography>
+              </ListItemText>
+            </ListItem>
+          ))}
         </List>
       </Container>
     </div>
